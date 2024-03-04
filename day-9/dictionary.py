a5a085af76d552d785c99e827b1c5f510013971c
Dj1236 <121332557+Dj1236@users.noreply.github.com>
2024-03-04 18:16:58 +0530

    Add files via upload

---
+++ day-9/dictionary.py
@@ -0,0 +1,9 @@
+programming_dictionary={
+    "Bug": "a error in a program that prevents the program running as expected.",
+    "Function": "A piece of code that you can easily call over and over again.",
+    "loop":"loop kya hai loop mai lappuu sa toh lup hai kuch aave jave hai na gol gol ghumta hai"
+}
+#print(programming_dictionary["Function"])
+#programming_dictionary["gaand"] = "sabko gand nai milti hai."
+programming_dictionary["Bug"]="a muth in your computer"
+print(programming_dictionary)
---
+++ day-9/nestinglist.py
@@ -0,0 +1,16 @@
+#nesting a list in the dictonary
+# trave_log ={
+#     "france" : ["paris","lille","dijon"],
+#     "germany" : ["berlin","hamburg","stuttagart"],
+# }
+#nesting dictionary in the dictonary
+# trave_log ={
+#     "france" :{"cities_visited" : ["paris","lille","dijon"],"total_visits": 12},
+#     "germany" : ["berlin","hamburg","stuttagart"],
+# }
+#dictonary in the list 
+trave_log =[
+    
+   {"country": "france","cities_visited" : ["paris","lille","dijon"],"total_visits":12}
+    "country ": ["berlin","hamburg","stuttagart"],
+] 
