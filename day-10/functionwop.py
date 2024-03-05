3fe547d4d3ebefecbef763ead3dbdeecf7204e09
Dj1236 <121332557+Dj1236@users.noreply.github.com>
2024-03-05 14:57:07 +0530

    Add files via upload

---
+++ day-10/functionwop.py
@@ -0,0 +1,9 @@
+def format_name(f_name,l_name):
+    if f_name == ""
+    formated_f_name = f_name.title()
+    formated_l_name = l_name.title()
+
+
+    return f"{formated_f_name}{formated_l_name}"
+
+format("DhAiRyA","MaHeK")      
